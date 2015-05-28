8677bf84b4abc8a6d970b6fdf4033320afc7c7e6
Tomas Hernandez <tomas.hernandez@ieee.org>
2015-05-27 22:04:26 -0700

    Added name field and protected controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528015759) do
+ActiveRecord::Schema.define(version: 20150528045720) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150528015759) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
