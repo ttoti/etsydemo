e85e68c90603941d4fb64b02dec3e9c555c3001b
Tomas Hernandez <tomas.hernandez@ieee.org>
2015-06-01 15:45:57 -0700

    Transfer funds to seller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -27,6 +27,8 @@ class OrdersController < ApplicationController
     @order.buyer_id = current_user.id
     @order.seller_id = @seller.id
 
+
+    require "stripe"
     Stripe.api_key = ENV["STRIPE_API_KEY"]
     token = params[:stripeToken]
 
@@ -40,6 +42,12 @@ class OrdersController < ApplicationController
     rescue Stripe::CardError => e
       flash[:danger] = e.message
     end
+
+    transfer = Stripe::Transfer.create(
+      :amount => (@listing.price * 95).floor,
+      :currency => "usd",
+      :recipient => @seller.recipient
+      )
 
     respond_to do |format|
       if @order.save
